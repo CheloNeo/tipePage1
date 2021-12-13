a694e543d572b9c68af1b104c0d3f4bbf223ae10
CheloNeo <85039594+CheloNeo@users.noreply.github.com>
2021-12-13 20:41:01 -0300

    cheloNeo
    cambio del title de la pagina

--- components/CustomHead/CustomHead.js
+++ components/CustomHead/CustomHead.js
@@ -5,13 +5,8 @@ const CustomHead = () => {
   return (
     <>
       <Head>
-        <title>MD Mujer</title>
-        <meta
-          name="description"
-          content="MD Mujer es una tienda de ropa femenina donde encontraras los mejores precios, las mejores
-            marcas, productos exclusivos y de tendencia, con envios a todo
-            Chile."
-        />
+        <title>Entregas Pro</title>
+        
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
