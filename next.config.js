8936b024d0bbddbcec73b5540a365fe4d232a1ba
CheloNeo <maemguitarra@gmail.com>
2021-12-13 19:30:17 -0300

    add heroku host

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
       "mongodb+srv://uv:uvpassword!@entregaspro.4mpwu.mongodb.net/EntregasPro?retryWrites=true&w=majority",
     JWT_SECRET:
       "d42c4d60f6de287cef4eaa3d544bf1b1e3e5857709ae2f1ab3b16a41d9f9a4eecc6f3f3270b02b033c3aa7f07a59dd6d7d9b7b27a0e5b3ca84d65472a3b16348",
-    HOST_URI: "http://localhost:3000",
+    HOST_URI: process.env.NODE_ENV === "production"? "https://entregaspro.herokuapp.com" : "http://localhost:3000",
   },
 };
