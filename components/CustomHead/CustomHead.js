962b4db4ad3ada8c16383c876da94cc79ef80607
CheloNeo <85039594+CheloNeo@users.noreply.github.com>
2021-12-13 21:03:32 -0300

    CheloNeo
    new commit

--- components/CustomHead/CustomHead.js
+++ components/CustomHead/CustomHead.js
@@ -6,7 +6,11 @@ const CustomHead = () => {
     <>
       <Head>
         <title>Entregas Pro</title>
-        <meta name="Descipcion" content="Entregas pro es una pyme que se dedica a la compra y venta de todo tipo de productos en la ciudad de la region de Valparaiso"/>
+        <meta
+          name="description"
+          content="Entregas pro es una pyme que se dedica a la entrega y venta de todo tipo de productos
+          tecnologicos dentro de la Calera!"
+        />
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
