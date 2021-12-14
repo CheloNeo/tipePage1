474ae65141129f9300da1709e823508188b882ad
CheloNeo <maemguitarra@gmail.com>
2021-12-13 22:01:32 -0300

    new commit

--- config/Cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: "entregaspro",
-  api_key: "345719359751421",
-  api_secret: "G3awlyLUJOe3exE8ksRpTgkgJ-g",
-});
-
-export default cloudinary;
