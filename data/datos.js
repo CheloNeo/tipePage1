9ea9623da1bbcd33237929a4bb63ad8c9b762f16
eliezer-zuniga <eliezer.zuniga@alumnos.uv.cl>
2021-12-13 23:11:27 -0300

    cambio de descripcion de empresa

--- data/datos.js
+++ data/datos.js
@@ -3,5 +3,5 @@ export const datos = {
   email_empresa: "entregas.pro@gmail.com",
   numero_empresa: 56948803985,
   descripcion_empresa:
-    "En nuestra tienda encontraras los mejores precios, productos exclusivos y de tendencia, con envios a toda la región de Valparaíso. Para dudas o consultas acerca de productos y stock, haz click aquí",
+    "En nuestra tienda encontraras los mejores precios, productos exclusivos y de tendencia, con envios a toda la región de Valparaíso. Para mas dudas o consultas haz click aquí",
 };
